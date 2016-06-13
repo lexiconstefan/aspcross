23c03a10fcdd56c5ea8209c2e6fc412b2a969e0a
stefan ferm <lexiconstefanferm@gmail.com>
2016-06-13 09:16:19 +0200

    new from lexicon

---
+++ Accounts.Web/Scripts/js/contact/controller.js
@@ -0,0 +1,10 @@
+﻿crmApp.controller('contactCtrl', ['$scope', function ($scope) {
+    var contact={};
+    contact.lastname = 'Ferm';
+    $scope.contact = contact;
+    $scope.submitContact = function () {
+        var t = "";
+    };
+  
+}]);
+
---
+++ Accounts.Web/Scripts/js/useraccount/editcontact.js
@@ -0,0 +1,28 @@
+﻿$(document).on('click', '.panel-heading span.clickable', function (e) {
+    var $this = $(this);
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    }
+}); $(document).ready(function () {
+    function setClasses(span) {
+        span.parents('.panel').find('.panel-body').slideUp();
+        span.addClass('panel-collapsed');
+        span.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down')
+    }
+    var width = $(window).width();
+   
+    if (width <= 480) {
+        setClasses( $('span.clickable'));
+       
+    } else if (width <= 768 && width > 480) {
+        setClasses( $('span#spanother'));
+       
+    }
+});
+
